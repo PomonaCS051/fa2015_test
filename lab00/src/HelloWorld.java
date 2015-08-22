d342f33e3c85d6e99e6b4aed5ef68eb3b71a59f2
Ross Wollman <rwoll@users.noreply.github.com>
2015-08-21 19:58:37 -0700

    resolve test push changes

--- lab00/src/HelloWorld.java
+++ lab00/src/HelloWorld.java
@@ -2,7 +2,7 @@ import objectdraw.*;
 
 public class HelloWorld extends WindowController {
 
-	private String yourName = "rwoll";
+	private String yourName = "<YourNameHere>";
 
 	public void begin() {
 		new Text("Hello, " + yourName, 10, 10, canvas);
